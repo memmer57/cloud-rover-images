823b4b059f7ba0470b07f1d0878504249ef6d4c3
Matěj Emmer <matej.emmer@gmail.com>
2023-06-16 13:28:12 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/homepage.scss";
+import { API_KEY } from "./helpers/API.js";
+import { useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState(null);
+
+  function fetchRoverImage() {
+    setLoading(true);
+    setImageUrl(null);
+    const rover = "perseverance";
+    // random sol
+    const sol = Math.floor(Math.random() * 1000);
+    const API_URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&api_key=${API_KEY}`;
+
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setImageUrl(data.photos[0].img_src);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setLoading(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="homepage">
+      <h1>Welcome to the Mars Rover Gallery</h1>
+      <p>Explore stunning images captured by the Mars rovers.</p>
+      <button className="fetch-button" onClick={() => fetchRoverImage()}>
+        Fetch Images
+      </button>
+      {loading && <h1>Loading...</h1>}
+      <div className="image-gallery">{imageUrl && <img src={imageUrl} />}</div>
     </div>
   );
 }
---
+++ src/helpers/API.js
@@ -0,0 +1,1 @@
+export const API_KEY = "0X6sQ6BT03iHhdPGLXZxXW5ethiUaiI0c7OTikmh";
